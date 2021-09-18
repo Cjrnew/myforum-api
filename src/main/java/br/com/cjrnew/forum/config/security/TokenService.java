36d968e54e752b7b09934211429ae2e740bf91af
Claudio Junior <Cjrnew@gmail.com>
2021-09-17 21:26:54 -0300

    Implementação Token JWT

---
+++ src/main/java/br/com/cjrnew/forum/config/security/TokenService.java
@@ -0,0 +1,49 @@
+package br.com.cjrnew.forum.config.security;
+
+import br.com.cjrnew.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("My Forum API")
+                .setSubject(logado.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(dataExpiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public Long getIdUsuario(String token) {
+        Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean isTokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+}
